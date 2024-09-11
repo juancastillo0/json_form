f7872535f42684c480d87110300494797378c22a
Juan Manuel Castillo <42351046+juancastillo0@users.noreply.github.com>
2024-09-11 14:32:00 -0500

    remove unused helpers

--- lib/src/helpers/helpers.dart
+++ lib/src/helpers/helpers.dart
@@ -1,19 +1,3 @@
-String? shift(List<String> elements) {
-  if (elements.isEmpty) return null;
-  return elements.removeAt(0);
-}
-
-Map<String, dynamic> merge(
-  Map<String, dynamic>? obj,
-  Map<String, dynamic> defaults,
-) {
-  if (obj == null) {
-    return defaults;
-  }
-  defaults.forEach((key, val) => obj.putIfAbsent(key, () => val));
-  return obj;
-}
-
 Object? copyJson(Object? json) {
   if (json is Map) {
     return json.map((key, value) => MapEntry(key, copyJson(value)));
